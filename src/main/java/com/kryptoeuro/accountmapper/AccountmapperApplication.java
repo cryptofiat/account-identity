a79d03182164f530fd47aa31f4a8f130d3699cb1
Alvar Lumberg <alvar@transferwise.com>
2016-07-30 00:38:14 +0300

    added dummy controller

--- src/main/java/com/kryptoeuro/accountmapper/AccountmapperApplication.java
+++ src/main/java/com/kryptoeuro/accountmapper/AccountmapperApplication.java
@@ -2,11 +2,19 @@ package com.kryptoeuro.accountmapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class AccountmapperApplication {
+@RestController
+public class AccountMapperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountmapperApplication.class, args);
+		SpringApplication.run(AccountMapperApplication.class, args);
 	}
+
+    @RequestMapping(value = "/", produces = "text/plain")
+    public String index() {
+        return "OK";
+    }
 }
