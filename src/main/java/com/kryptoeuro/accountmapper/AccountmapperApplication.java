adb92712a1e908a1362e7c0348b8172e36dfc06e
Alvar Lumberg <alvar@transferwise.com>
2016-07-29 23:54:57 +0300

    initial commit

---
+++ src/main/java/com/kryptoeuro/accountmapper/AccountmapperApplication.java
@@ -0,0 +1,12 @@
+package com.kryptoeuro.accountmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmapperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kryptoeuro/accountmapper/AccountmapperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kryptoeuro.accountmapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountmapperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
