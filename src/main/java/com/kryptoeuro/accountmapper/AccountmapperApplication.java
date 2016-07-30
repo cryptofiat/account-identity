566f359b7b557e3a24c1f4cdd52a4f607252c597
Alvar Lumberg <alvar@transferwise.com>
2016-07-30 10:21:36 +0300

    adds dummy account list and creation methods

--- src/main/java/com/kryptoeuro/accountmapper/AccountmapperApplication.java
+++ src/main/java/com/kryptoeuro/accountmapper/AccountmapperApplication.java
@@ -1,13 +1,21 @@
 package com.kryptoeuro.accountmapper;
 
+import com.kryptoeuro.accountmapper.domain.EthereumAccount;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class AccountMapperApplication {
+
+    @Autowired
+    EthereumAccountRepository ethereumAccountRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMapperApplication.class, args);
@@ -16,5 +24,17 @@ public class AccountMapperApplication {
     @RequestMapping(value = "/", produces = "text/plain")
     public String index() {
         return "OK";
+    }
+
+    @RequestMapping(value = "/account/create")
+    public EthereumAccount createAccount(@RequestParam(name = "ownerId") String ownerId) {
+        EthereumAccount account = new EthereumAccount(ownerId, "0xDEADBEEF");
+        ethereumAccountRepository.save(account);
+        return account;
+    }
+
+    @RequestMapping(value = "/account/list")
+    public List<EthereumAccount> listAccounts(@RequestParam(name = "ownerId") String ownerId) {
+        return ethereumAccountRepository.findByOwnerId(ownerId);
     }
 }
