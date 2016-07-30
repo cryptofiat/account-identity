ef6a6432aeab030bc701366fb852996a6376072b
ErikK <erik@transferwise.com>
2016-07-30 18:04:35 +0300

    Moved /test endpoints to TestController

--- src/main/java/com/kryptoeuro/accountmapper/AccountMapperApplication.java
+++ src/main/java/com/kryptoeuro/accountmapper/AccountMapperApplication.java
@@ -1,6 +1,5 @@
 package com.kryptoeuro.accountmapper;
 
-import com.codeborne.security.mobileid.MobileIDSession;
 import com.kryptoeuro.accountmapper.domain.EthereumAccount;
 import com.kryptoeuro.accountmapper.mobileid.MobileIdAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @SpringBootApplication
@@ -57,30 +55,5 @@ public class AccountMapperApplication {
     @RequestMapping(value = "/account/list")
     public List<EthereumAccount> listAccounts(@RequestParam(name = "ownerId") String ownerId) {
         return ethereumAccountRepository.findByOwnerId(ownerId);
-    }
-
-//  Use 60000007 for testing
-    @RequestMapping(value = "/test/mobileid")
-    public String testMobileIdLogin(@RequestParam(name = "mobileNumber") String mobileNumber) {
-        MobileIDSession mobileIDlogin = mobileIdAuthService.login(mobileNumber);
-
-        return mobileIDlogin != null
-                ? "FirstName: " + mobileIDlogin.firstName +
-                    "\nLastName: " + mobileIDlogin.lastName +
-                    "\nID: " + mobileIDlogin.personalCode
-                : "Could not login";
-    }
-
-    @RequestMapping(value = "/test/addToSession")
-    public String addToSession(@RequestParam(name = "objectValue") String value, HttpSession httpSession) {
-        httpSession.removeAttribute("sessionObject");
-        httpSession.setAttribute("sessionObject", value);
-        return "OK";
-    }
-
-    @RequestMapping(value = "/test/getFromSession")
-    public String getFromSession(HttpSession httpSession) {
-        Object value = httpSession.getAttribute("sessionObject");
-        return value != null ? value.toString() : "Value has not found in session. Use /test/addToSession?sessionObject=yourvalue to add it.";
     }
 }
---
+++ src/main/java/com/kryptoeuro/accountmapper/playground/TestController.java
@@ -0,0 +1,44 @@
+package com.kryptoeuro.accountmapper.playground;
+
+import com.codeborne.security.mobileid.MobileIDSession;
+import com.kryptoeuro.accountmapper.mobileid.MobileIdAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    MobileIdAuthService mobileIdAuthService;
+
+//  Use 60000007 or 53477254 for testing
+//  https://demo.sk.ee/MIDCertsReg/index.php <- register your phone number here
+    @RequestMapping(value = "/mobileid")
+    public String testMobileIdLogin(@RequestParam(name = "mobileNumber") String mobileNumber) {
+        MobileIDSession mobileIDlogin = mobileIdAuthService.login(mobileNumber);
+
+        return mobileIDlogin != null
+                ? "FirstName: " + mobileIDlogin.firstName +
+                    "\nLastName: " + mobileIDlogin.lastName +
+                    "\nID: " + mobileIDlogin.personalCode
+                : "Could not login";
+    }
+
+    @RequestMapping(value = "/addToSession")
+    public String addToSession(@RequestParam(name = "objectValue") String value, HttpSession httpSession) {
+        httpSession.removeAttribute("sessionObject");
+        httpSession.setAttribute("sessionObject", value);
+        return "OK";
+    }
+
+    @RequestMapping(value = "/getFromSession")
+    public String getFromSession(HttpSession httpSession) {
+        Object value = httpSession.getAttribute("sessionObject");
+        return value != null ? value.toString() : "Value has not found in session. Use /test/addToSession?sessionObject=yourvalue to add it.";
+    }
+}
