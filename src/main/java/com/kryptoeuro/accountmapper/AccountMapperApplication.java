a9b2fc9498a62770466d5c5e314a97a76715fc90
Erik Jõgi <erik.jogi@gmail.com>
2016-07-30 16:56:22 +0300

    pseudocode for mobile id authentication flow

--- src/main/java/com/kryptoeuro/accountmapper/AccountMapperApplication.java
+++ src/main/java/com/kryptoeuro/accountmapper/AccountMapperApplication.java
@@ -30,11 +30,27 @@ public class AccountMapperApplication {
         return "OK";
     }
 
-    @RequestMapping(value = "/account/create")
-    public EthereumAccount createAccount(@RequestParam(name = "ownerId") String ownerId) {
-        EthereumAccount account = new EthereumAccount(ownerId, "0xDEADBEEF");
+    @RequestMapping(value = "/authenticate")
+    public String authenticate(@RequestParam(name = "address") String address, @RequestParam(name = "phoneNumber") String phoneNumber) {
+        // start mobile auth
+        // save MobileIDSession in HTTP session
+        // save address in HTTP session
+        // return MobileIDSession.challenge;
+        return "";
+    }
+
+    @RequestMapping(value = "/poll")
+    public String poll() {
+        // get MobileIDSession from HTTP session
+        // call MobileIDAuthentocator.isLoginComplete()
+        // if true then accountAuthenticated(address from HTTP session, personalCode from mobile id session)
+        return ""; // ok / nok
+    }
+
+    void accountAuthenticated(String address, String ownerId) {
+        EthereumAccount account = new EthereumAccount(address, ownerId);
         ethereumAccountRepository.save(account);
-        return account;
+        // activate account in ethereum contract
     }
 
     @RequestMapping(value = "/account/list")
