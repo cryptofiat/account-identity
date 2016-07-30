05534d8d8d7055f0e1762b24e43a28acffbb2f6d
ErikK <erik@transferwise.com>
2016-07-30 18:32:40 +0300

    cleaned AccountMapperApplication and moved api calls to AccountMapperController where /v1/authenticate/ is already json POST

--- src/main/java/com/kryptoeuro/accountmapper/AccountMapperApplication.java
+++ src/main/java/com/kryptoeuro/accountmapper/AccountMapperApplication.java
@@ -1,24 +1,13 @@
 package com.kryptoeuro.accountmapper;
 
-import com.kryptoeuro.accountmapper.domain.EthereumAccount;
-import com.kryptoeuro.accountmapper.mobileid.MobileIdAuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class AccountMapperApplication {
-
-    @Autowired
-    EthereumAccountRepository ethereumAccountRepository;
-    @Autowired
-    MobileIdAuthService mobileIdAuthService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMapperApplication.class, args);
@@ -27,34 +16,5 @@ public class AccountMapperApplication {
     @RequestMapping(value = "/", produces = "text/plain")
     public String index() {
         return "OK";
-    }
-
-    @RequestMapping(value = "/authenticate")
-    public String authenticate(@RequestParam(name = "address") String address, @RequestParam(name = "phoneNumber") String phoneNumber, HttpSession session) {
-        // start mobile auth
-        // save MobileIDSession in HTTP session
-        // save address in HTTP session
-        session.setAttribute("address", address);
-        // return MobileIDSession.challenge;
-        return "";
-    }
-
-    @RequestMapping(value = "/poll")
-    public String poll() {
-        // get MobileIDSession from HTTP session
-        // call MobileIDAuthentocator.isLoginComplete()
-        // if true then accountAuthenticated(address from HTTP session, personalCode from mobile id session)
-        return ""; // ok / nok
-    }
-
-    void accountAuthenticated(String address, String ownerId) {
-        EthereumAccount account = new EthereumAccount(address, ownerId);
-        ethereumAccountRepository.save(account);
-        // activate account in ethereum contract
-    }
-
-    @RequestMapping(value = "/account/list")
-    public List<EthereumAccount> listAccounts(@RequestParam(name = "ownerId") String ownerId) {
-        return ethereumAccountRepository.findByOwnerId(ownerId);
     }
 }
---
+++ src/main/java/com/kryptoeuro/accountmapper/rest/AccountMapperController.java
@@ -0,0 +1,69 @@
+package com.kryptoeuro.accountmapper.rest;
+
+import com.kryptoeuro.accountmapper.EthereumAccountRepository;
+import com.kryptoeuro.accountmapper.command.AuthenticateCommand;
+import com.kryptoeuro.accountmapper.domain.EthereumAccount;
+import com.kryptoeuro.accountmapper.mobileid.MobileIdAuthService;
+import com.kryptoeuro.accountmapper.response.AuthenticateResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+@RequestMapping("/v1")
+public class AccountMapperController {
+	@Autowired
+	EthereumAccountRepository ethereumAccountRepository;
+	@Autowired
+	MobileIdAuthService mobileIdAuthService;
+
+	@RequestMapping(value = "/", produces = "text/plain")
+	public String index() {
+		return "OK";
+	}
+
+	@RequestMapping(
+			method = POST,
+			value = "/authenticate/",
+			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<AuthenticateResponse> authenticate(@Valid @RequestBody AuthenticateCommand authenticateCommand, HttpSession session) {
+		// start mobile auth
+		// save MobileIDSession in HTTP session
+		// save address in HTTP session
+		session.setAttribute("address", authenticateCommand.getAccountAddress());
+		// return MobileIDSession.challenge;
+
+		AuthenticateResponse authenticateResponse = new AuthenticateResponse("1337");
+		return new ResponseEntity<AuthenticateResponse>(authenticateResponse, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/poll")
+	public String poll() {
+		// get MobileIDSession from HTTP session
+		// call MobileIDAuthentocator.isLoginComplete()
+		// if true then accountAuthenticated(address from HTTP session, personalCode from mobile id session)
+		return ""; // ok / nok
+	}
+
+	void accountAuthenticated(String address, String ownerId) {
+		EthereumAccount account = new EthereumAccount(address, ownerId);
+		ethereumAccountRepository.save(account);
+		// activate account in ethereum contract
+	}
+
+	@RequestMapping(value = "/account/list")
+	public List<EthereumAccount> listAccounts(@RequestParam(name = "ownerId") String ownerId) {
+		return ethereumAccountRepository.findByOwnerId(ownerId);
+	}
+}
