7ae00a2f3106edd885d169e004a2d474205d0ae9
ErikK <erik@transferwise.com>
2016-07-30 16:50:10 +0300

    logs [debug] mobile id auth attempts

--- src/main/java/com/kryptoeuro/accountmapper/mobileid/MobileIdAuthService.java
+++ src/main/java/com/kryptoeuro/accountmapper/mobileid/MobileIdAuthService.java
@@ -3,10 +3,12 @@ package com.kryptoeuro.accountmapper.mobileid;
 import com.codeborne.security.AuthenticationException;
 import com.codeborne.security.mobileid.MobileIDAuthenticator;
 import com.codeborne.security.mobileid.MobileIDSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @Transactional(rollbackFor = Exception.class)
 public class MobileIdAuthService {
 
@@ -21,15 +23,14 @@ public class MobileIdAuthService {
 	public MobileIDSession login(String phoneNumber) {
 		MobileIDSession mobileIDSession;
 		try {
-//			log.debug("Mobile ID authentication with challenge " + mobileIDSession.challenge + " sent to " + phoneNumber);
 			mobileIDSession = mid.startLogin(phoneNumber);
+			log.debug("Mobile ID authentication with challenge " + mobileIDSession.challenge + " sent to " + phoneNumber);
 			mid.waitForLogin(mobileIDSession);
-//			log.debug("Mobile ID authentication success! First name: " + mobileIDSession.firstName + ", Last name: " + mobileIDSession.lastName + ", Personal code: " + mobileIDSession.personalCode);
-
+			log.debug("Mobile ID authentication success! First name: " + mobileIDSession.firstName + ", Last name: " + mobileIDSession.lastName + ", Personal code: " + mobileIDSession.personalCode);
 		} catch (AuthenticationException e) {
 			e.printStackTrace();
+			log.debug("Mobile ID authentication failed" + e.getMessage());
 			return null;
-//			log.debug("Mobile ID authentication failed" + e.getMessage());
 		}
 
 		return mobileIDSession;
