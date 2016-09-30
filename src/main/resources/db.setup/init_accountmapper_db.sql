a9d3f3808e080cd9727011576a951b7daa0ed23a
ErikK <erik@transferwise.com>
2016-09-30 23:34:06 +0300

    update initial db schema script

--- src/main/resources/db.setup/init_accountmapper_db.sql
+++ src/main/resources/db.setup/init_accountmapper_db.sql
@@ -1,8 +1,10 @@
-CREATE SCHEMA `accountmapper` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ CREATE SCHEMA `account_identity` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Might fail if flyway user has already been created for that node
 CREATE USER 'flyway'@'localhost' IDENTIFIED BY 'q1w2e3r4';
-GRANT ALL PRIVILEGES ON accountmapper.* TO 'flyway'@'localhost';
+GRANT ALL PRIVILEGES ON account_identity.* TO 'flyway'@'localhost';
 
-CREATE USER 'accountmapper-service'@'localhost' IDENTIFIED BY 'q1w2e3r4';
-GRANT ALL PRIVILEGES ON accountmapper.* TO 'accountmapper-service'@'localhost';
+CREATE USER 'acc-id-service'@'localhost' IDENTIFIED BY 'q1w2e3r4';
+GRANT ALL PRIVILEGES ON account_identity.* TO 'acc-id-service'@'localhost';
+
+USE account_identity;
