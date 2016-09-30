8de233578f13e1954d777116b3d2a62869e2e1b8
ErikK <erik@transferwise.com>
2016-10-01 00:46:29 +0300

    move initial schema and db init scripts

--- src/main/resources/db/migration/V1_1__initial_schema.sql
+++ src/main/resources/db/migration/V1_1__initial_schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS ethereum_account;
 CREATE TABLE ethereum_account (
-  id BIGINT(20) NOT NULL,
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
   owner_id VARCHAR(25) NOT NULL,
   address VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
