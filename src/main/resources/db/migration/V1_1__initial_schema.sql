880ab559b914cfd3e187a9b78b2083fa5e7339ca
ErikK <erik@transferwise.com>
2016-10-01 01:16:13 +0300

    do not drop initial table on app start

--- src/main/resources/db/migration/V1_1__initial_schema.sql
+++ src/main/resources/db/migration/V1_1__initial_schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS ethereum_account;
+-- DROP TABLE IF EXISTS ethereum_account;
 CREATE TABLE ethereum_account (
   id BIGINT(20) NOT NULL AUTO_INCREMENT,
   owner_id VARCHAR(25) NOT NULL,
