afdf9248757fc1cbb58e500d9335a15ccdbeabf6
ErikK <erik@transferwise.com>
2016-10-02 12:37:25 +0300

    add request and response bodies for payment reference generation

---
+++ src/main/java/com/kryptoeuro/accountmapper/command/GetPaymentReferenceCommand.java
@@ -0,0 +1,16 @@
+package com.kryptoeuro.accountmapper.command;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class GetPaymentReferenceCommand {
+	@NotNull
+	UUID authIdentifier;
+	@NotNull
+	String signedAuthIdentifier;
+}
