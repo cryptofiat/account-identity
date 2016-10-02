af94a78656ad360edfc5cd381ba2d26a50afeb20
ErikK <erik@transferwise.com>
2016-10-02 11:21:03 +0300

    update parity node url

--- src/main/java/com/kryptoeuro/accountmapper/service/EthereumService.java
+++ src/main/java/com/kryptoeuro/accountmapper/service/EthereumService.java
@@ -37,7 +37,7 @@ public class EthereumService {
 //	private String contractAddress = "0xAF8ce136A244dB6f13a97e157AC39169F4E9E445"; // contract 0.31
 //	private String contractAddress = "0x2FdAB8f12fA9Ad9Ad91fc55d52569AFc98Be9831"; // contract 0.41
 	private String contractAddress = "0xC91F200C33de61FF7B9B930968d0B59C1b86DAf9"; // versionless
-	private String jsonRpcUrl = "http://eth.euro2.ee:8080"; // Parity node on AWS
+	private String jsonRpcUrl = "http://46.4.80.11:8545"; // Parity node on AWS
 
 	private Function approveAccountFunction = Function.fromSignature("approveAccount", "address");
 	private Function appointAccountApproverFunction = Function.fromSignature("appointAccountApprover", "address");
