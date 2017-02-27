85d19c4d2c3ed6f09316e12027556746f159f487
Kristo Kaarmann <kristo.kaarmann@gmail.com>
2017-02-27 21:40:55 +0000

    search idcode from LDAP cache by name

--- src/main/java/com/kryptoeuro/accountmapper/LdapResponseRepository.java
+++ src/main/java/com/kryptoeuro/accountmapper/LdapResponseRepository.java
@@ -2,6 +2,9 @@ package com.kryptoeuro.accountmapper;
 
 import com.kryptoeuro.accountmapper.response.LdapResponse;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,4 +13,12 @@ public interface LdapResponseRepository extends CrudRepository<LdapResponse,Long
     List<LdapResponse> findByIdCode(Long idCode);
 
     void delete(Long id);
+
+    // cheap-o alternative to lucene or elasticsearch
+    @Query("SELECT r FROM LdapResponse r WHERE "
+		+ "r.firstName LIKE '%' || :searchString || '%' "
+		+ "OR r.lastName LIKE '%' || :searchString || '%' "
+		+ "OR r.idCode LIKE '%' || :searchString || '%' ")
+    List<LdapResponse> searchLdapResponse(@Param("searchString") String searchString, Pageable pageable);
+
 }
--- src/main/java/com/kryptoeuro/accountmapper/rest/LdapController.java
+++ src/main/java/com/kryptoeuro/accountmapper/rest/LdapController.java
@@ -16,6 +16,10 @@ import lombok.extern.slf4j.Slf4j;
 import com.kryptoeuro.accountmapper.response.LdapResponse;
 import com.kryptoeuro.accountmapper.service.LdapService;
 import com.kryptoeuro.accountmapper.error.LdapNotFoundException;
+import com.kryptoeuro.accountmapper.error.SearchTooBroadException;
+import com.kryptoeuro.accountmapper.LdapResponseRepository;
+import org.springframework.data.domain.PageRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/ldap")
@@ -25,7 +29,11 @@ public class LdapController {
 
 	@Autowired
 	LdapService ldapService;
+	@Autowired
+	LdapResponseRepository ldapResponseRepository;
 
+	private static int MIN_SEARCH_QUERY_LENGTH = 3;
+	private static int MAX_RESULT_SIZE = 10;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{idCode}")
 	public ResponseEntity<LdapResponse> checkIdCode(@PathVariable("idCode") long idCode) {
@@ -36,5 +44,15 @@ public class LdapController {
 		} else {
 			throw new LdapNotFoundException();
 		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/search")
+	public ResponseEntity<List<LdapResponse>> search( @RequestParam("searchString") String searchString) {
+		if ( searchString.length() < MIN_SEARCH_QUERY_LENGTH) {
+			throw new SearchTooBroadException();
+		}
+
+		List<LdapResponse> lr = ldapResponseRepository.searchLdapResponse(searchString, new PageRequest(0,MAX_RESULT_SIZE));
+		return new ResponseEntity<List<LdapResponse>>(lr, HttpStatus.OK);
 	}
 }
