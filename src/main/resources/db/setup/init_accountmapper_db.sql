f24324a1673d57012543aa916faf56daf9499ce4
ErikK <erik@transferwise.com>
2017-03-17 21:17:23 +0200

    added some comments to init sql

--- src/main/resources/db/setup/init_accountmapper_db.sql
+++ src/main/resources/db/setup/init_accountmapper_db.sql
@@ -2,9 +2,11 @@ CREATE SCHEMA `account_identity` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_u
 
 -- Might fail if flyway user has already been created for that node
 CREATE USER 'flyway'@'localhost' IDENTIFIED BY 'q1w2e3r4';
+-- if access denied on app startup, try the line below without @'localhost'
 GRANT ALL PRIVILEGES ON account_identity.* TO 'flyway'@'localhost';
 
 CREATE USER 'acc-id-service'@'localhost' IDENTIFIED BY 'q1w2e3r4';
+-- if access denied on app startup, try the line below without @'localhost'
 GRANT ALL PRIVILEGES ON account_identity.* TO 'acc-id-service'@'localhost';
 
 USE account_identity;
