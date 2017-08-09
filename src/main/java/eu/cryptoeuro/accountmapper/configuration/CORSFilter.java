b507d99522fbb3bcb24e7c7134f8df01ba20cb88
Kristo Kaarmann <kristo.kaarmann@gmail.com>
2017-08-09 20:29:26 +0100

    fix for CORS

--- src/main/java/eu/cryptoeuro/accountmapper/configuration/CORSFilter.java
+++ src/main/java/eu/cryptoeuro/accountmapper/configuration/CORSFilter.java
@@ -25,11 +25,11 @@ public class CORSFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://wallet.euro2.ee");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-//        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
 
         List<String> allowedHeaders = Arrays.asList(
                 "x-requested-with",
